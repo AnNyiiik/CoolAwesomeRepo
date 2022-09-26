8fa0b09478d997a7eaa4b133f18babf6e7ab7a61
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-09-26 23:48:04 +0300

    main.c

--- SmartQsort/Lol/main.c
+++ SmartQsort/Lol/main.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define SIZE_DATA 100000
+
 void insertionSort(int *data, int start, int end)
 {
     for (int i = start + 1; i <= end; i++)
@@ -48,7 +54,34 @@ void smartQuickSort(int *data, int start, int end) {
     smartQuickSort(data, left, end);
 }
 
+bool testQuickSort(void) {
+    int data[100] = {0};
+    for (int i = 0; i < 100; ++i) {
+        data[i] = rand() % 100 - 50;
+    }
+    smartQuickSort(data, 0, 99);
+    for (int i = 0; i < 99; ++i) {
+        if (data[i] > data[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
+    if(!testQuickSort()) {
+        printf("%s", "Tests are incorrect!\n");
+        return 1;
+    }
+    printf("%s", "Tests are correct!\n");
+    int data[SIZE_DATA] = {0};
+    for (int i = 0; i < SIZE_DATA; ++i) {
+        data[i] = rand() % 10000 - 5000;
+    }
+    smartQuickSort(data, 0, SIZE_DATA - 1);
+    for (int i = 0; i < SIZE_DATA; ++i) {
+        printf("%d%s", data[i], " ");
+    }
     return 0;
 }
 

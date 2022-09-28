9b3f4667d8b9e1a0281826432842900dcac86b11
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-09-28 14:47:28 +0300

    MostFrequentElement

--- MostFrequentElement/main.c
+++ MostFrequentElement/main.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+int findMinValue(int * data, int size) {
+    int minValue = data[0];
+    for (int i = 0; i < size; ++i) {
+        if (data[i] < minValue) {
+            minValue = data[i];
+        }
+    }
+    return minValue;
+}
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int findMaxValue(int * data, int size) {
+    int maxValue = data[0];
+    for (int i = 0; i < size; ++i) {
+        if (data[i] > maxValue) {
+            maxValue = data[i];
+        }
+    }
+    return maxValue;
 }

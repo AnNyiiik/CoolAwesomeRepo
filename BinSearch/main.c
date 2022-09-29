e2538deb8582f3b4b9947050b9e43bff2f69b120
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-09-29 20:33:08 +0300

    Added insertion sort

--- BinSearch/main.c
+++ BinSearch/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+void insertionSort(int *data, int start, int end)
+{
+    for (int i = start + 1; i <= end; i++)
+    {
+        int index = i - 1;
+        while(index >= start && (data[index] > data[index + 1]))
+        {
+            data[index + 1] = data[index + 1] ^ data[index];
+            data[index] = data[index] ^ data[index + 1];
+            data[index + 1] = data[index + 1] ^ data[index];
+            index--;
+        }
+    }
+}
+
 int main() {
     printf("Hello, World!\n");
     return 0;

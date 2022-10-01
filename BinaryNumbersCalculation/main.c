a941e3f869b3a4c20ce866dfecbcdbb161f764d3
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-02 01:02:17 +0300

    Added sum function

--- BinaryNumbersCalculation/main.c
+++ BinaryNumbersCalculation/main.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <locale.h>
 
-void binaryRepresentation(int number, char *binaryNumber) {
-    int bit = 0b10000000;
-
-    for (int i = 0; i < 8; ++i) {
-        binaryNumber[7 - i] = (bit & number) ? 1 : 0;
+void binaryRepresentation(int number, int *binaryNumber) {
+    int bit = 0b01000000000000000000000000000000;
+    binaryNumber[0] = (number < 0) ? 1 : 0;
+    for (int i = 1; i < 32; ++i) {
+        *(binaryNumber + i) = (bit & number) ? 1 : 0;
         bit = bit >> 1;
     }
-    for (int i = 7; i >= 0; --i) {
-        printf("%d", binaryNumber[i]);
+}
+
+void binaryAddition(int *firstNumber, int *secondNumber, int *result) {
+    int carrier = 0;
+    for (int i = 31; i >= 0; --i) {
+        result[i] = (((firstNumber[i] + secondNumber[i] + carrier) % 2) == 1) ? 1 : 0;
+        carrier = ((firstNumber[i] + secondNumber[i] + carrier) > 1) ? 1 : 0;
     }
+
     return;
 }
+
 int main() {
     setlocale(LC_ALL, "Rus");
     printf("Введите два числа:\n");
     int firstNumber = 0;
-    //int secondNumber = 0;
-    scanf("%d", &firstNumber);
-    int *binaryNumber[8] = {0};
-    binaryRepresentation(firstNumber, binaryNumber);
-
+    int secondNumber = 0;
+    scanf("%d%d", &firstNumber, &secondNumber);
+    int binaryFirstNumber[32] = {0};
+    binaryRepresentation(firstNumber, binaryFirstNumber);
+    for (int i = 0; i < 32; ++i) {
+        printf("%d", binaryFirstNumber[i]);
+    }
+    printf("%s", "\n");
+    int binarySecondNumber[32] = {0};
+    binaryRepresentation(secondNumber, binarySecondNumber);
+    for (int i = 0; i < 32; ++i) {
+        printf("%d", binarySecondNumber[i]);
+    }
+    printf("%s", "\n");
+    int result[32] = {0};
+    binaryAddition(binaryFirstNumber, binarySecondNumber, result);
+    for (int i = 0; i < 32; ++i) {
+        printf("%d", result[i]);
+    }
     return 0;
 }

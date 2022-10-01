2a8d4fa44036402fbad4d4e7458990d3766974b8
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-01 22:41:12 +0300

    Added reversion function 10 -> 2

--- BinaryNumbersCalculation/main.c
+++ BinaryNumbersCalculation/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <locale.h>
 
+void binaryRepresentation(int number, char *binaryNumber) {
+    int bit = 0b10000000;
+
+    for (int i = 0; i < 8; ++i) {
+        binaryNumber[7 - i] = (bit & number) ? 1 : 0;
+        bit = bit >> 1;
+    }
+    for (int i = 7; i >= 0; --i) {
+        printf("%d", binaryNumber[i]);
+    }
+    return;
+}
 int main() {
-    printf("Hello, World!\n");
+    setlocale(LC_ALL, "Rus");
+    printf("Введите два числа:\n");
+    int firstNumber = 0;
+    //int secondNumber = 0;
+    scanf("%d", &firstNumber);
+    int *binaryNumber[8] = {0};
+    binaryRepresentation(firstNumber, binaryNumber);
+
     return 0;
 }

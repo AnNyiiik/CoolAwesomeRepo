e15a09c45da0555c5da0b6ae6e9d589ffd71561e
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-03 10:25:35 +0300

    Added mostFrequentElementFUnction

--- ModuleSort/main.c
+++ ModuleSort/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "countingSort.h"
+
+int mostFrequentElement(int * data, int size) {
+    int minimalValue = findMinValue(data, size);
+    int maximalValue = findMaxValue(data, size);
+    int * countArray = (int*) calloc(maximalValue - minimalValue + 1, sizeof(int));
+    countingSort(countArray, maximalValue - minimalValue + 1);
+    int maximum = 0;
+    int maximumIndex = 0;
+    for (int i = 0; i < maximalValue - minimalValue + 1; ++i) {
+        if (countArray[i] > maximumIndex) {
+            maximum = i + minimalValue;
+            maximumIndex = countArray[i];
+        }
+    }
+    free(countArray);
+    return maximum;
+}
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
 }

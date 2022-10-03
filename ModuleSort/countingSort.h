edc626d08f3af7073a17c9776d054136cc8404e5
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-03 10:17:27 +0300

    Added declarations of findMinElement, findMaxElement, countingSort

---
+++ ModuleSort/countingSort.h
@@ -0,0 +1,6 @@
+#ifndef MODULESORT_QUICKSORT_H
+#define MODULESORT_QUICKSORT_H
+    int findMinValue(int *data, int size);
+    int findMaxValue(int *data, int size);
+    int countingSort(int *data, int size);
+#endif //MODULESORT_QUICKSORT_H

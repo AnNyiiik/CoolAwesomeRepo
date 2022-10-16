8f4903ea02998c11de6a0e79e39470dbd1c03d10
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-16 11:33:14 +0300

    changed file

--- StackModule/main.c
+++ StackModule/main.c
@@ -2,8 +2,8 @@
 #include "stack.h"
 int main() {
     Node *head = NULL;
-    pushBack(&head, 1);
-    pushBack(&head, 2);
-    pushBack(&head, 3);
+    pushBack(&head, "a");
+    pushBack(&head, "b");
+    pushBack(&head, "c");
     deleteAll(&head);
 }

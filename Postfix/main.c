a7043fee589441b11540a8523369b4d4e92c814b
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-16 12:32:12 +0300

    added some code

--- Postfix/main.c
+++ Postfix/main.c
@@ -11,7 +11,40 @@ int main() {
     int size = strlen(postfixExpression);
     for (int i = 0; i < size; ++i) {
         if ((int) postfixExpression[i] < 10 && (int) postfixExpression[i] >= 0) {
-            pushBack(&head, (int) postfixExpression);
+            pushBack(&head, postfixExpression[i]);
+        } else {
+            int temporaryValue = 0;
+            int returnValue = 0;
+            int errorCode = 0;
+            if (postfixExpression[i] == '+') {
+                for (int i = 0; i < 2; ++i) {
+                    errorCode = pop(&head, &returnValue);
+                    if (errorCode != 0) {
+                        return 1;
+                    }
+                    temporaryValue = temporaryValue + returnValue;
+                }
+                errorCode = pushBack(&head, temporaryValue);
+                if (errorCode != 0) {
+                    return 1;
+                }
+            } else if (postfixExpression[i] == '-') {
+                int sign = 1;
+                for (int i = 0; i < 2; ++i) {
+                    errorCode = pop(&head, &returnValue);
+                    if (errorCode != 0) {
+                        return 1;
+                    }
+                    temporaryValue = temporaryValue + returnValue * sign;
+                    sign = -1;
+                }
+                errorCode = pushBack(&head, temporaryValue);
+                if (errorCode != 0) {
+                    return 1;
+                }
+            } else if (postfixExpression[i] == '*') {
+
+            }
         }
     }
 }

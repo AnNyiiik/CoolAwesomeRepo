a67572170885a3099038ea5ac0a916fb0018658f
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-16 11:29:35 +0300

    added empty tests to .c

---
+++ StackModule/test.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include "test.h"
+
+bool testPush(void) {
+    return false;
+}
+
+bool testPop(void) {
+    return false;
+}
+
+bool testDeleteAll(void) {
+    return false;
+}
+
+bool testIsEmpty(void) {
+    return false;
+}

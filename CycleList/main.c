70f609fadb42c14fd5d93815d7267bf49594bf6d
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-26 00:36:26 +0300

    added tests for list

--- CycleList/main.c
+++ CycleList/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include "cycleList.h"
+#include "test.h"
 int main() {
+    if (!testCreate()) {
+        return 1;
+    }
+    if (!testPushBack()) {
+        return 1;
+    }
+    if (!testPop()) {
+        return 1;
+    }
+    if (!testDelete()) {
+        return 1;
+    }
+    if (!testIsEmpty()) {
+        return 1;
+    }
     List *list = createList();
     pushBack(&list, 1);
     pushBack(&list, 2);

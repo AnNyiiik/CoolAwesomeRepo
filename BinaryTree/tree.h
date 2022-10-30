8f4aa7837f0609842487607e98d6970d94e97c96
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-30 16:08:14 +0300

    added clear

--- BinaryTree/tree.h
+++ BinaryTree/tree.h
@@ -13,5 +13,7 @@ int findValue(int key, BinaryTree *tree, bool *isExits, char *value);
 
 int deleteElement(int key, BinaryTree **tree);
 
-int clearTree(BinaryTree **tree);
+int clear(BinaryTree **tree);
+
+int clearTree(Node **root);
 #endif //BINARYTREE_TREE_H

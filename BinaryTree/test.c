e0374903daf936be7e0003ec9fc0038f5f84f75f
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-10-30 16:33:54 +0300

    added test files

---
+++ BinaryTree/test.c
@@ -0,0 +1,20 @@
+#include "test.h"
+bool testCreateTree(void) {
+    return false;
+}
+
+bool testAddElement(void) {
+    return false;
+}
+
+bool testFind(void) {
+    return false;
+}
+
+bool testDeleteElement(void) {
+    return false;
+}
+
+bool testClear(void) {
+    return false;
+}
---
+++ BinaryTree/test.h
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#ifndef BINARYTREE_TEST_H
+#define BINARYTREE_TEST_H
+bool testCreateTree(void);
+
+bool testAddElement(void);
+
+bool testFind(void);
+
+bool testDeleteElement(void);
+
+bool testClear(void);
+#endif //BINARYTREE_TEST_H

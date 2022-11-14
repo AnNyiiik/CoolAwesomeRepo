2324c84865e59450e6a1f4b2c0b160c26c932018
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-11-14 10:48:36 +0300

    changed

--- List/list.c
+++ List/list.c
@@ -1,22 +1,25 @@
 #include "list.h"
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 typedef struct ListElement {
-    int value;
+    int frequency;
+    char *word;
     struct ListElement *next;
 } ListElement;
 
 typedef struct List {
     struct ListElement *head;
+    struct ListElement *tail;
     int size;
 } List;
 
 List *createList(void) {
     struct List *list = (List *) malloc(sizeof(List));
     list->head = NULL;
+    list->tail = NULL;
     list->size = 0;
     return list;
 }
@@ -24,47 +27,11 @@ List *createList(void) {
 int deleteList(List **list) {
     while (!isEmpty(*list)) {
         int value = 0;
-        int errorCode = pop(list, &value);
+        int errorCode = pop(list);
         if (errorCode != 0) {
             return 1;
         }
     }
-    return 0;
-}
-
-int insert(List *list, int place, int value) {
-    if (isEmpty(list)) {
-        int errorCode = push(&list, value);
-        if (errorCode != 0) {
-            return 1;
-        }
-        return 0;
-    }
-    if (place > list->size) {
-        return 1;
-    }
-    if (place == 0) {
-        int errorCode = push(&list, value);
-        if (errorCode != 0) {
-            return 1;
-        }
-        return 0;
-    }
-    int index = 0;
-    ListElement *element = list->head;
-    while (index < place - 1 && element->next) {
-        element = element->next;
-        ++index;
-    }
-    ListElement *newElement = (ListElement *) malloc(sizeof(ListElement));
-    newElement->value = value;
-    if (element->next) {
-        newElement->next = element->next;
-    } else {
-        newElement->next = NULL;
-    }
-    element->next = newElement;
-    ++list->size;
     return 0;
 }
 
@@ -76,8 +43,7 @@ int delete(List *list, int place) {
         return 1;
     }
     if (place == 0) {
-        int value = 0;
-        int errorCode = pop(&list, &value);
+        int errorCode = pop(&list);
         if (errorCode != 0) {
             return 1;
         }
@@ -92,116 +58,70 @@ int delete(List *list, int place) {
     if (element->next) {
         if (element->next->next) {
             ListElement *elementAfterDeleted = element->next->next;
+            free(element->next->word);
             free(element->next);
             element->next = elementAfterDeleted;
             --list->size;
             return 0;
+        } else {
+            free(element->next->word);
+            free(element->next);
         }
-        free(element->next);
         element->next = NULL;
         --list->size;
         return 0;
     }
-    --list->size;
     return 0;
 }
 
-int getElementPlace(List *list, int value) {
+int getElementPlace(List *list, char *word) {
     if (isEmpty(list)) {
         return -1;
     }
     ListElement *element = list->head;
-    int index = 0;
+    int place = 0;
     while (element->next) {
-        if (element->value == value) {
-            return index;
+        if (strcmp(word, element->word)) {
+            return place;
         }
         element = element->next;
-        ++index;
+        ++place;
     }
     return -1;
 }
 
-int insertByOrder(List *list, int value) {
-    if (isEmpty(list)) {
-        int errorCode = push(&list, value);
-        if (errorCode != 0) {
-            return 1;
-        }
-        return 0;
-    }
-    if (list->head->value >= value) {
-        int errorCode = push(&list, value);
-        if (errorCode != 0) {
-            return 1;
-        }
-        return 0;
-    }
-    ListElement *element = list->head;
-    ListElement *previous = list->head;
-    while (element->value <= value && element->next) {
-        previous = element;
-        element = element->next;
-    }
-    if ((!element->next) && (element->value <= value)) {
-        ListElement *newElement = (ListElement *) malloc(sizeof(ListElement));
-        newElement->value = value;
-        newElement->next = NULL;
-        element->next = newElement;
-        ++list->size;
-        return 0;
-    }
-    ListElement *newElement = (ListElement *) malloc(sizeof(ListElement));
-    newElement->value = value;
-    if (previous->next) {
-        newElement->next = previous->next;
-    } else {
-        newElement->next = NULL;
-    }
-    previous->next = newElement;
-    ++list->size;
-    return 0;
-}
-
-int push(List **list, int value) {
+int push(List **list, int frequency, char *word) {
     ListElement *newNode = (ListElement *) malloc(sizeof(ListElement));
     if (newNode == NULL) {
         return 1;
     }
-    newNode->value = value;
-    newNode->next = (*list)->head;
-    (*list)->head = newNode;
+    newNode->frequency = frequency;
+    newNode = (char *) malloc(sizeof(word));
+    strcpy(newNode->word, word);
+    if ((*list)->head) {
+        (*list)->tail->next = newNode;
+        newNode->next = NULL;
+        (*list)->tail = newNode;
+    } else {
+        newNode->next = NULL;
+        (*list)->head = newNode;
+    }
     ++(*list)->size;
     return 0;
 }
 
-int pop(List **list, int *value) {
+int pop(List **list) {
     if (isEmpty(list)) {
-        return 1;
+        return 0;
     }
     ListElement *previous = (*list)->head;
-    *value = (previous)->value;
     ((*list)->head) = ((*list)->head)->next;
     --(*list)->size;
+    free(previous->word);
     free(previous);
     return 0;
 }
 
 bool isEmpty(List *list) {
     return list->head == NULL;
-}
-
-void printList(List *list) {
-    if (isEmpty(list)) {
-        printf("List is empty\n");
-    }
-    ListElement *element = list->head;
-    while (element != NULL) {
-        if (!element->next) {
-            printf("%d%s", element->value, "\n");
-        } else {
-            printf("%d%s", element->value, ", ");
-        }
-        element = element->next;
-    }
 }

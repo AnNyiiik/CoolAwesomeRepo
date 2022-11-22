e6b3593bd2863071a8ef454d96898c607188459a
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-11-22 21:43:25 +0300

    added file input

--- Countries/cmake-build-debug/matrixFunctions.h
+++ Countries/cmake-build-debug/matrixFunctions.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #ifndef COUNTRIES_MATRIXFUNCTIONS_H
 #define COUNTRIES_MATRIXFUNCTIONS_H
-typedef struct Data Data;
-Data *readFromFile(FILE *file);
+typedef struct Data {
+    int roads;
+    int towns;
+    int **matrix;
+    int *capitals;
+    int numberOfCapitals;
+} Data;
+int readFromFile(FILE *file, Data **data);
 #endif //COUNTRIES_MATRIXFUNCTIONS_H

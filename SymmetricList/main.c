21facd358e66d711f717a51ca940299d412a3602
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-11-23 17:01:00 +0300

    done the task

--- SymmetricList/main.c
+++ SymmetricList/main.c
@@ -3,30 +3,37 @@
 #include "../List/list.h"
 #include "../BinaryTree/tree.h"
 
-bool testSymmetric(void) {
+bool testSymmetric(int *errorCode) {
     FILE *file = fopen("../testFile", "r");
     if (file == NULL) {
+        *errorCode = 1;
         return false;
     }
     char *number = (char *)malloc(sizeof(char) * 30);
     if (number == NULL) {
         fclose(file);
-        return 1;
+        *errorCode = 1;
+        return false;
     }
     List *list = createList();
     while ((fscanf(file, "%s", number)) > 0) {
-        int errorCode = pushFront(&list, atoi(number));
+        *errorCode = pushFront(&list, atoi(number));
         if (errorCode == 1) {
             fclose(file);
+            free(number);
             deleteList(&list);
-            return 1;
+            *errorCode = 1;
+            return false;
         }
     }
     fclose(file);
     free(number);
+    *errorCode = 0;
     if (checkSymmetric(list)) {
+        deleteList(&list);
         return false;
     }
+    deleteList(&list);
     return true;
 }
 
@@ -38,8 +45,28 @@ BinaryTree *createSortingTree(int *data, int size) {
     return tree;
 }
 
+bool testSort(void) {
+    int testData[10] = {0, -19, 77, 25, -16, 9, 45, 82, -100, -4};
+    int correctResult[10] = {-100, -19, -16, -4, 0, 9, 25, 45, 77, 82};
+    BinaryTree *tree = createSortingTree(testData, 10);
+    sortTree(tree, testData, 10);
+    for (int i = 0; i < 10; ++i) {
+        if (testData[i] != correctResult[i]) {
+            clear(&tree);
+            return false;
+        }
+    }
+    clear(&tree);
+    return true;
+}
+
 int main() {
-    if (!testSymmetric()) {
+    int errorCode = 0;
+    if (!testSymmetric(&errorCode) && errorCode == 0) {
+        printf("test have been failed");
+        return 1;
+    } else if (errorCode == 1) {
+        printf("problems :((");
         return 1;
     }
     FILE *file = fopen("../text", "r");
@@ -49,6 +76,7 @@ int main() {
     }
     char *number = (char *)malloc(sizeof(char) * 30);
     if (number == NULL) {
+        fclose(file);
         return 1;
     }
     List *list = createList();
@@ -59,13 +87,24 @@ int main() {
     free(number);
     bool answer = checkSymmetric(list);
     if (answer) {
-        printf("List is symmetric");
+        printf("List is symmetric\n");
     } else {
-        printf("List isn't symmetric");
+        printf("List isn't symmetric\n");
     }
     deleteList(&list);
-    int *data[5] = {9, 16, -12, 1, 6};
+    if (!testSort()) {
+        return 1;
+    }
+    int data[5] = {9, 16, -12, 1, 6};
     BinaryTree *tree = createSortingTree(data, 5);
-    sortTree(tree);
+    errorCode = sortTree(tree, data, 5);
+    if (errorCode == 1) {
+        clear(&tree);
+        return 1;
+    }
+    for (int i = 0; i < 5; ++i) {
+        printf("%d%s", data[i], " ");
+    }
+    clear(&tree);
     return 0;
 }

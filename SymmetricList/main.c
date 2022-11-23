fd1c711ec1f6c8afec8ed59e48ab2b1b6362f7b4
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-11-23 16:19:46 +0300

    done this task

--- SymmetricList/main.c
+++ SymmetricList/main.c
@@ -2,13 +2,46 @@
 #include <stdlib.h>
 #include "../List/list.h"
 
+bool testSymmetric(void) {
+    FILE *file = fopen("../testFile", "r");
+    if (file == NULL) {
+        return false;
+    }
+    char *number = (char *)malloc(sizeof(char) * 30);
+    if (number == NULL) {
+        fclose(file);
+        return 1;
+    }
+    List *list = createList();
+    while ((fscanf(file, "%s", number)) > 0) {
+        int errorCode = pushFront(&list, atoi(number));
+        if (errorCode == 1) {
+            fclose(file);
+            deleteList(&list);
+            return 1;
+        }
+    }
+    fclose(file);
+    free(number);
+    if (checkSymmetric(list)) {
+        return false;
+    }
+    return true;
+}
+
 int main() {
+    if (!testSymmetric()) {
+        return 1;
+    }
     FILE *file = fopen("../text", "r");
     if (file == NULL) {
         printf("File not found");
         return 1;
     }
     char *number = (char *)malloc(sizeof(char) * 30);
+    if (number == NULL) {
+        return 1;
+    }
     List *list = createList();
     while ((fscanf(file, "%s", number)) > 0) {
         pushFront(&list, atoi(number));

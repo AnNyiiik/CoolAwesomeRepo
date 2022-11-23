c314ed024b56c47754ef180fc20a08838b6708de
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-11-23 16:11:07 +0300

    added 1 func

--- SymmetricList/main.c
+++ SymmetricList/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../List/list.h"
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *file = fopen("../text", "r");
+    if (file == NULL) {
+        printf("File not found");
+        return 1;
+    }
+    char *number = (char *)malloc(sizeof(char) * 30);
+    List *list = createList();
+    while ((fscanf(file, "%s", number)) > 0) {
+        pushFront(&list, atoi(number));
+    }
+    fclose(file);
+    free(number);
+    bool answer = checkSymmetric(list);
+    if (answer) {
+        printf("List is symmetric");
+    } else {
+        printf("List isn't symmetric");
+    }
+    deleteList(&list);
     return 0;
 }

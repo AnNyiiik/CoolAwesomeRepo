5f9bf8079be26ec3733b7ca1deb20bca0ec4a90d
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-12-07 17:45:35 +0300

    changed

--- ProgressCheck2.1/main.c
+++ ProgressCheck2.1/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../List/list.h"
+#include "../List/test.h"
 
 int main() {
-    printf("Hello, World!\n");
+    if (!testIsEmpty() || !testPop() || !testCreate() || !testDelete() || !testInsertByOrder() || !testInsert()) {
+        return 1;
+    }
+    printf("enter a and b values (a > b):\n");
+    int boundFirst = 0;
+    int boundSecond = 0;
+    scanf("%d", &boundFirst);
+    scanf("%d", &boundSecond);
+    if (boundFirst > boundSecond) {
+        return 1;
+    }
+    FILE *fileInput = fopen("../input.txt", "r");
+    if (fileInput == NULL) {
+        return 1;
+    }
+    List *listAB = createList();
+    List *listLow = createList();
+    List *listUp = createList();
+    char number[15] = {0};
+    while (fscanf(fileInput, "%s", number) > 0) {
+        int element = atoi(number);
+        if (element >= boundFirst && element <= boundSecond) {
+            push(&listAB, element);
+        } else if (element > boundSecond) {
+            push(&listUp, element);
+        } else {
+            push(&listLow, element);
+        }
+    }
     return 0;
 }

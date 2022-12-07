e60d69dabf99424cfc1ead8a3d452460b0ff0a03
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-12-07 18:45:00 +0300

    changed

--- ProgressCheck2.1/main.c
+++ ProgressCheck2.1/main.c
@@ -3,21 +3,20 @@
 #include "../List/list.h"
 #include "../List/test.h"
 
-int main() {
-    if (!testIsEmpty() || !testPop() || !testCreate()) {
-        return 1;
+typedef enum ErrorCode {
+    fileNotFound,
+    wrongInput,
+    listError,
+    ok
+} ErrorCode;
+
+ErrorCode reformatFileData(const char *path, List **list, int boundFirst, int boundSecond) {
+    if (boundSecond < boundFirst) {
+        return wrongInput;
     }
-    printf("enter a and b values (a > b):\n");
-    int boundFirst = 0;
-    int boundSecond = 0;
-    scanf("%d", &boundFirst);
-    scanf("%d", &boundSecond);
-    if (boundFirst > boundSecond) {
-        return 1;
-    }
-    FILE *fileInput = fopen("../input", "r");
+    FILE *fileInput = fopen(path, "r");
     if (fileInput == NULL) {
-        return 1;
+        return fileNotFound;
     }
     List *listAB = createList();
     List *listLow = createList();
@@ -25,20 +24,97 @@ int main() {
     char number[15] = {0};
     while (fscanf(fileInput, "%s", number) > 0) {
         int element = atoi(number);
+        int error = 0;
         if (element >= boundFirst && element <= boundSecond) {
-            pushBack(&listAB, element);
+            error = pushBack(&listAB, element);
         } else if (element > boundSecond) {
-            pushBack(&listUp, element);
+            error = pushBack(&listUp, element);
         } else {
-            pushBack(&listLow, element);
+            error = pushBack(&listLow, element);
+        }
+        if (error == 1) {
+            fclose(fileInput);
+            deleteList(&listAB);
+            deleteList(&listUp);
+            deleteList(&listLow);
+            return listError;
         }
     }
     fclose(fileInput);
-    List *list = makeOne(makeOne(listLow, listAB), listUp);
-    printList(list, "../output");
-    deleteList(&listUp);
-    deleteList(&listAB);
-    deleteList(&listLow);
+    *list = makeOne(makeOne(listLow, listAB), listUp);
+    int error = 0;
+    error = deleteList(&listUp);
+    if (error == 1) {
+        deleteList(&listAB);
+        deleteList(&listLow);
+        return listError;
+    }
+    error = deleteList(&listAB);
+    if (error == 1) {
+        deleteList(&listUp);
+        deleteList(&listLow);
+        return listError;
+    }
+    error = deleteList(&listLow);
+    if (error == 1) {
+        deleteList(&listAB);
+        deleteList(&listUp);
+        return listError;
+    }
+    return ok;
+}
+
+bool test() {
+    List *list = NULL;
+    ErrorCode errorCode = reformatFileData("../testData", &list, -1, 5);
+    if (errorCode != ok) {
+        return false;
+    }
+    printList(list,"../testOutput");
     deleteList(&list);
+    int correctSequence[10] = {-3, -2, -1, 5, 2, 3, 10, 8, 14, 6};
+    int numbersRead = 0;
+    char number[15] = {0};
+    FILE *fileInput = fopen("../testOutput", "r");
+    if (fileInput == NULL) {
+        return false;
+    }
+    while (fscanf(fileInput, "%s", number) > 0) {
+        if (numbersRead == 10) {
+            ++numbersRead;
+            break;
+        }
+        int element = atoi(number);
+        if (element != correctSequence[numbersRead]) {
+            break;
+        }
+        ++numbersRead;
+    }
+    fclose(fileInput);
+    return numbersRead == 10;
+}
+
+int main() {
+    if (!testIsEmpty() || !testPop() || !testCreate()) {
+        printf("Some problems with list");
+        return 1;
+    }
+    if (!test()) {
+        printf("Test has been failed");
+        return 1;
+    }
+    printf("enter a and b values (a <= b):\n");
+    int boundFirst = 0;
+    int boundSecond = 0;
+    scanf("%d", &boundFirst);
+    scanf("%d", &boundSecond);
+    List *list = NULL;
+    reformatFileData("../input", &list, boundFirst, boundSecond);
+    printList(list, "../output");
+    int error = deleteList(&list);
+    if (error == 1) {
+        printf("Problems with delete");
+        return 1;
+    }
     return 0;
 }

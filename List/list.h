e7410b2e9f257997cc530668617bc7e4bca68537
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-12-07 10:01:54 +0300

    add commets to list.h

--- List/list.h
+++ List/list.h
@@ -1,23 +1,33 @@
-#include <stdbool.h>
 #ifndef LIST_LIST_H
 #define LIST_LIST_H
+
+#include <stdbool.h>
+
 typedef struct ListElement ListElement;
 
 typedef struct List List;
 
+//Create an empty list.
 List *createList(void);
 
+//Delete List with all its elements.
 int deleteList(List **list);
 
+//Add a new element at the head.
 int push(List **list, int value);
 
+//Insert a new element into the List by place
 int insert(List *list, int place, int value);
 
+//Insert a new element into the List by sorted order according to a value.
 int insertByOrder(List *list, int value);
 
+//Delete an element by place.
 int delete(List *list, int place);
 
+//Delete an element from the head.
 int pop(List **list, int *value);
 
+//Check if the list is empty.
 bool isEmpty(List *list);
 #endif //LIST_LIST_H

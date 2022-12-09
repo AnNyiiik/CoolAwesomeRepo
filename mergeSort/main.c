25b78fce5423f4bde7387f83975eb3942683e331
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-12-09 15:39:12 +0300

    fixed

--- mergeSort/main.c
+++ mergeSort/main.c
@@ -1,44 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 #include "test.h"
 int main() {
-    if (!testCreate()) {
-        return 1;
-    }
-    if (!testDelete()) {
-        return 1;
-    }
-    if (!testPushBack()) {
-        return 1;
-    }
-    if (!testPop()) {
-        return 1;
-    }
-    if (!testIsEmpty()) {
-        return 1;
-    }
-    if (!testSort()) {
+    if (!testCreate() || !testDelete() || !testPushBack() || !testPop() || !testIsEmpty() || !testSort()) {
         return 1;
     }
     List *list = createList();
-    FILE *file = fopen("/Users/annnikolaeff/CoolAwesomeRepo/mergeSort/people&phones", "r");
+    FILE *file = fopen("../people&phones", "r");
     if (file == NULL) {
         printf("%s", "Файл не найден!");
         return 1;
     }
     while (!feof(file)) {
-        char *bufferName = malloc(sizeof(char) * 100);
-        char *bufferNumber = malloc(sizeof(char) * 30);
+        char bufferName[100] = {0};
+        char bufferNumber[30] = {0};
         const int readBytes = fscanf(file, "%s%s", bufferName, bufferNumber);
         if (readBytes < 0) {
-            free(bufferName);
-            free(bufferNumber);
             break;
         }
         pushBack(&list, bufferName, bufferNumber);
-        free(bufferName);
-        free(bufferNumber);
     }
     fclose(file);
     printf("Enter 0 to sort by name, 1 - by phone:\n");

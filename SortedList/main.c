422ed0c13a40543e55c90a6dd7bdfdbdd0699e3f
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-12-09 15:47:38 +0300

    changed

--- SortedList/main.c
+++ SortedList/main.c
@@ -2,22 +2,7 @@
 #include "../List/list.h"
 #include "../List/test.h"
 int main() {
-    if (!testCreate()) {
-        return 1;
-    }
-    if (!testDelete()) {
-        return 1;
-    }
-    if (!testInsert()) {
-        return 1;
-    }
-    if (!testPop()) {
-        return 1;
-    }
-    if (!testInsertByOrder()) {
-        return 1;
-    }
-    if (!testIsEmpty()) {
+    if (!testCreate() || !testDelete() || !testInsert() || !testPop() || !testInsertByOrder() || !testIsEmpty()) {
         return 1;
     }
     int option = 0;

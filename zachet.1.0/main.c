ee66bc0beb5dbb8bd88906ef9ae467e73a76f102
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-12-14 15:58:52 +0300

    done 1st

---
+++ zachet.1.0/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int convertToDecimal(const char *binaryString, int *result) {
+    if (strlen(binaryString) > 32) {
+        return 1;
+    } else if (strlen(binaryString) == 32 && binaryString[0] == '1') {
+        return 1;
+    } else if (strlen(binaryString) == 0) {
+        return 1;
+    }
+    int base = 1;
+    *result = 0;
+    int length = strlen(binaryString);
+    for (int i = length - 1; i >= 0; --i) {
+        *result += base * (binaryString[i] - '0');
+        base = base * 2;
+    }
+    return 0;
+}
+
+bool testConversion(void) {
+    int result = 0;
+    convertToDecimal("101", &result);
+    if (result != 5) {
+        return false;
+    }
+    convertToDecimal("1010", &result);
+    if (result != 10) {
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    if (!testConversion()) {
+        return 1;
+    }
+    int result = 0;
+    convertToDecimal("101010", &result);
+    printf("The result is %d\n", result);
+    return 0;
+}

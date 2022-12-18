6e5b83e9c0725ebd66cb252308d80221281301b5
AnNyiiik <114094098+AnNyiiik@users.noreply.github.com>
2022-12-18 16:46:47 +0300

    changed

--- BinaryTree/test.h
+++ BinaryTree/test.h
@@ -1,10 +1,15 @@
-#include <stdbool.h>
 #ifndef BINARYTREE_TEST_H
 #define BINARYTREE_TEST_H
+
+#include <stdbool.h>
+
+// Test function createTree.
 bool testCreateTree(void);
 
+// Test function addElement.
 bool testAddElement(void);
 
+// Test function findValue.
 bool testFind(void);
 
 bool testDeleteElement(void);
